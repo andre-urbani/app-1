7f4135509b0e29baa2cbce50c6c4e819a061ecce
dredizzle <dredizzle@live.co.uk>
2020-03-12 15:09:26 +0000

    implementing fetch request and search bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
-// import { HashLink as Link } from 'react-router-hash-link'
+import axios from 'axios'
 
 const Navbar = () => {
+
+  const [searches, setSearches] = useState()
+
+  const handleSearch = (e) => {
+    e.target.value !== '' ?
+      axios.get(`https://api.teleport.org/api/cities/?search=${e.target.value}`)
+        .then(res => setSearches(res.data.data.slice(0, 10)))
+        .catch(err => console.log(err))
+      : setSearches('')
+
+  }
 
   return <div className="navbar-container">
     <div>Home</div>
@@ -10,7 +21,7 @@ const Navbar = () => {
     <div>Cities</div>
     <div>Urban Area Scores</div>
     <div className="search-bar">
-      <input type="text" placeholder="Search for a city..."></input>
+      <input type="text" placeholder="Search for a city..." onChange={handleSearch}></input>
     </div>
     </div>
 
