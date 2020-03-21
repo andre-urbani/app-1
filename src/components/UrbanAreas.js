080a8d6a9b973f533e4b5b3fc70cadf0f9470313
dredizzle <dredizzle@live.co.uk>
2020-03-21 21:00:20 +0000

    added urban areas page

---
+++ src/components/UrbanAreas.js
@@ -0,0 +1,37 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import axios from 'axios'
+
+const UrbanAreas = () => {
+
+  const uAreas = 'ua:item'
+
+  const [urbanAreas, setUrbanAreas] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.teleport.org/api/urban_areas/')
+      .then(res => {
+        const urbanAreaList = res.data._links[uAreas]
+        setUrbanAreas(urbanAreaList)
+      })
+  }, [])
+
+  return <div className="continents-container">
+    {console.log(urbanAreas)}
+
+    <div>
+      {urbanAreas.map((urbanArea, i) => {
+        return <div key={i}>{urbanArea.name}
+        {/* <div onClick={handleClick}>List of countries</div>
+      <div>{countryList}</div> */}
+        {/* <div>list of urban areas</div> */}
+        </div>
+      })}
+    </div>
+
+
+  </div>
+
+
+}
+
+export default UrbanAreas
