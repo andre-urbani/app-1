c0e11c643fd91beff3e903bc200c226160715e72
dredizzle <dredizzle@live.co.uk>
2020-03-24 14:56:34 +0000

    specific country page

--- src/components/Country.js
+++ src/components/Country.js
@@ -12,33 +12,42 @@ const Country = (props) => {
       .then(res => {
         setCountry(res.data)
         axios.get(`${props.location.target}salaries`)
-        .then(res => {
-          const salaryList = res.data.salaries
-          setSalaries(salaryList)
-      })
+          .then(res => {
+            const salaryList = res.data.salaries
+            setSalaries(salaryList)
+          })
       })
   }, [])
 
   return <div className="continents-container">
     {console.log(country.iso_alpha2)}
     <div>
-     {country.name}
+      {country.name}
     </div>
     <div>
-     Population: {country.population}
+      Population: {country.population}
     </div>
     <div>
-     Currency: {country.currency_code}
+      Currency: {country.currency_code}
     </div>
     <div>
-    {salaries.map((salary, i) => {
-          return <div key={i}>{salary.job.title} </div>
-        })}
-    
+      {salaries.map((salary, i) => {
+        return <div key={i}>{salary.job.title} </div>
+      })}
+
     </div>
     {country.iso_alpha2 ?
-    <img src={`https://www.countryflags.io/${country.iso_alpha2}/shiny/64.png`}/>
-    : null}
+      <img src={`https://www.countryflags.io/${country.iso_alpha2}/shiny/64.png`} />
+      : null}
+
+    <div class="dropdown">
+      <button class="dropbtn">Dropdown</button>
+      <div class="dropdown-content">
+      {salaries.map((salary, i) => {
+        return <a key={i}>{salary.job.title}</a>
+      })}
+      </div>
+    </div>
   </div>
 
 
