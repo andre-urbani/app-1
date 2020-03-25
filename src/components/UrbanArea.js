3c56371c8aa1902ee76a38e32631075f798512b8
dredizzle <dredizzle@live.co.uk>
2020-03-25 17:07:59 +0000

    individual urban area page

--- src/components/UrbanArea.js
+++ src/components/UrbanArea.js
@@ -10,33 +10,36 @@ const UrbanArea = (props) => {
 
   const [urbanAreaImage, setUrbanAreaImage] = useState([])
 
+  const [imageJson, setImageJson] = useState([])
+
   useEffect(() => {
     axios.get(props.location.target)
       .then(res => {
         const uaList = res.data._links
         setUrbanArea(uaList)
-        axios.get(urbanArea[image])
-        .then(res => {
-          const uaImage = res.data._links[image]
-          setUrbanAreaImage(uaImage)
-          
-          
-        })
-        
+        axios.get(props.location.target)
+          .then(res => {
+            const uaImage = res.data._links[image].href
+            setUrbanAreaImage(uaImage)
+            axios.get(urbanAreaImage)
+              .then(res => {
+                setImageJson(res.data)
+              })
+          })
       })
   }, [])
 
   return <div>
     {/* {console.log(urbanArea[image])} */}
-    {console.log(urbanAreaImage)}
-   {/* {urbanArea ?
+    {console.log(imageJson)}
+    {/* {urbanArea ?
     
-     <div><img src={urbanAreaImage[image].href} /> 
+     <div><img src={urbanAreaImage.photos[0].image.web} /> 
      </div> :
    null } */}
   </div>
 
-  
+
 
 }
 
