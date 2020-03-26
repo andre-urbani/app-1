b34ac941efce3fda5ce42b60736fb1db17734b99
dredizzle <dredizzle@live.co.uk>
2020-03-26 19:48:51 +0000

    individual urban area page

--- src/components/Continent.js
+++ src/components/Continent.js
@@ -2,39 +2,23 @@ import React, { useState, useCallback, useEffect } from 'react';
 import axios from 'axios'
 
 
-const UrbanArea = (props) => {
+const Continent = (props) => {
 
-  const image = 'ua:images'
-
-  const [urbanArea, setUrbanArea] = useState([])
-
-  const [imageJson, setImageJson] = useState([])
+  const [continent, setContinent] = useState([])
 
   useEffect(() => {
     axios.get(props.location.target)
       .then(res => {
         const uaList = res.data._links
         setUrbanArea(uaList)
-        
-            axios.get(res.data._links[image].href)
-              .then(res => {
-                setImageJson(res.data.photos[0].image)
-              })
           })
   }, [])
 
   return <div>
-    {/* {console.log(urbanArea[image])} */}
-    {console.log(imageJson)}
-    {imageJson ?
-    
-     <div><img src={imageJson.mobile} /> 
-     </div> :
-   null }
   </div>
 
 
 
 }
 
-export default UrbanArea
+export default Continent
