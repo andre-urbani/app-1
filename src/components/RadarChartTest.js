9a820fb76e9d4656dec2f8994136897b7a9fd21d
dredizzle <dredizzle@live.co.uk>
2020-04-01 16:33:16 +0100

    testing recharts

--- src/components/RadarChartTest.js
+++ src/components/RadarChartTest.js
@@ -3,72 +3,51 @@ import {
   Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis,
 } from 'recharts';
 
-const data = [
-  {
-    subject: 'Housing', A: 10, fullMark: 10,
-  },
-  {
-    subject: 'Cost of Living', A: 8.2, fullMark: 10,
-  },
-  {
-    subject: 'Startups', A: 4.6, fullMark: 10,
-  },
-  {
-    subject: 'Venture Capital', A: 7, fullMark: 10,
-  },
-  {
-    subject: 'Travel Connectivity', A: 6.8, fullMark: 10,
-  },
-  {
-    subject: 'Commute', A: 6, fullMark: 10,
-  },
-  {
-    subject: 'Business Freedom', A: 3, fullMark: 10,
-  },
-  {
-    subject: 'Safety', A: 8, fullMark: 10,
-  },
-  {
-    subject: 'Healthcare', A: 4, fullMark: 10,
-  },
-  {
-    subject: 'Education', A: 7.3, fullMark: 10,
-  },
-  {
-    subject: 'Environmental Quality', A: 6.8, fullMark: 10,
-  },
-  {
-    subject: 'Internet Access', A: 5.55, fullMark: 10,
-  },
-  {
-    subject: 'Taxation', A: 9.2, fullMark: 10,
-  },
-  {
-    subject: 'Economy', A: 8.3, fullMark: 10,
-  },
-  {
-    subject: 'Leisure & Culture', A: 5.4, fullMark: 10,
-  },
-  {
-    subject: 'Tolerance', A: 4, fullMark: 10,
-  },
-  {
-    subject: 'Outdoor', A: 7, fullMark: 10,
-  },
-  
-];
+const RadaChartTest = (props) => {
 
-export default class Example extends PureComponent {
-  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/6ebcxbx4/';
+  const data = [
+    {
+      subject: 'Housing', A: 2, fullMark: 15,
+    },
+    {
+      subject: 'Cost of Living', A: 8.2, fullMark: 15,
+    },
+    {
+      subject: 'Commute', A: 6, fullMark: 15,
+    },
+    {
+      subject: 'Safety', A: 8, fullMark: 15,
+    },
+    {
+      subject: 'Education', A: 7.3, fullMark: 15,
+    },
+    {
+      subject: 'Environmental Quality', A: 6.8, fullMark: 15,
+    },
+    {
+      subject: 'Economy', A: 8.3, fullMark: 15,
+    },
+    {
+      subject: 'Leisure & Culture', A: 5.4, fullMark: 15,
+    },
+    {
+      subject: 'Tolerance', A: 4, fullMark: 15,
+    },
+    {
+      subject: 'Outdoor', A: 7, fullMark: 15,
+    },
 
-  render() {
-    return (
-      <RadarChart cx={300} cy={250} outerRadius={150} width={800} height={800} data={data}>
-        <PolarGrid />
-        <PolarAngleAxis dataKey="subject" />
-        <PolarRadiusAxis />
-        <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
-      </RadarChart>
-    );
-  }
+  ];
+
+  return (
+    <RadarChart cx={300} cy={250} outerRadius={150} width={800} height={800} data={data} >
+      <PolarGrid />
+      <PolarAngleAxis dataKey="subject" />
+      <PolarRadiusAxis />
+      <Radar dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+    </RadarChart>
+  );
 }
+
+export default RadaChartTest
+
--- src/components/UrbanArea.js
+++ src/components/UrbanArea.js
@@ -1,42 +1,53 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import axios from 'axios'
-import GraphTestTwo from './GraphTestTwo';
+import RadarChartTest from './RadarChartTest';
 
 
 const UrbanArea = (props) => {
 
   const image = 'ua:images'
 
+  const score = 'ua:scores'
+
   const [urbanArea, setUrbanArea] = useState([])
 
   const [imageJson, setImageJson] = useState([])
 
-  const [test, setTest] = useState({
-    internet: 9.432
-  })
+  const [test, setTest] = useState([])
+
+
 
   useEffect(() => {
     axios.get(props.location.target)
       .then(res => {
         const uaList = res.data._links
         setUrbanArea(uaList)
-        
-            axios.get(res.data._links[image].href)
+        axios.get(res.data._links[image].href)
+          .then(res => {
+            setImageJson(res.data.photos[0].image)
+            const scoreUrl = urbanArea[score].href
+            axios.get(scoreUrl)
               .then(res => {
-                setImageJson(res.data.photos[0].image)
+                setTest(res.data)
               })
           })
+      })
   }, [])
 
   return <div>
     {/* {console.log(urbanArea[image])} */}
-    {console.log(imageJson)}
+    {console.log(urbanArea[score])}
     {imageJson ?
-    
-     <div><img src={imageJson.mobile} /> 
-     </div> :
-   null }
-   <GraphTestTwo test={test}/>
+
+      <div><img src={imageJson.mobile} />
+      </div> :
+      null}
+    <RadarChartTest test={test} />
+    {test ?
+
+      <div>hello
+      </div> :
+      null}
   </div>
 
 
