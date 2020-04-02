3edde1ec1e2f6540f1cd08726789d0c7d9abe7be
dredizzle <dredizzle@live.co.uk>
2020-04-02 13:25:17 +0100

    testing recharts

--- src/components/RadarChartTest.js
+++ src/components/RadarChartTest.js
@@ -17,6 +17,15 @@ const RadaChartTest = (props) => {
       axios.get(props.urbanArea[score].href)
         .then(res => {
           setHousing(res.data.categories[0])
+          setCostOfLiving(res.data.categories[1])
+          setCommute(res.data.categories[5])
+          setSafety(res.data.categories[7])
+          setEducation(res.data.categories[9])
+          setEnvironment(res.data.categories[10])
+          setEconomy(res.data.categories[11])
+          setLeisure(res.data.categories[14])
+          setTolerance(res.data.categories[15])
+          setOutdoor(res.data.categories[16])
         })
     }
   }, [props.urbanArea[score]])
@@ -24,37 +33,47 @@ const RadaChartTest = (props) => {
   
 
   const [housing, setHousing] = useState([]) 
+  const [costOfLiving, setCostOfLiving] = useState([])
+  const [commute, setCommute] = useState([])
+  const [safety, setSafety] = useState([])
+  const [education, setEducation] = useState([])
+  const [environment, setEnvironment] = useState([])
+  const [economy, setEconomy] = useState([])
+  const [leisure, setLeisure] = useState([])
+  const [tolerance, setTolerance] = useState([])
+  const [outdoor, setOutdoor] = useState([])
+
 
   const data = [
     {
       subject: housing.name, A: housing.score_out_of_10, fullMark: 15,
     },
     {
-      subject: 'Cost of Living', A: 8.2, fullMark: 15,
+      subject: costOfLiving.name, A: costOfLiving.score_out_of_10, fullMark: 15,
     },
     {
-      subject: 'Commute', A: 6, fullMark: 15,
+      subject: commute.name, A: commute.score_out_of_10, fullMark: 15,
     },
     {
-      subject: 'Safety', A: 8, fullMark: 15,
+      subject: safety.name, A: safety.score_out_of_10, fullMark: 15,
     },
     {
-      subject: 'Education', A: 7.3, fullMark: 15,
+      subject: education.name, A: education.score_out_of_10, fullMark: 15,
     },
     {
-      subject: 'Environmental Quality', A: 6.8, fullMark: 15,
+      subject: environment.name, A: environment.score_out_of_10, fullMark: 15,
     },
     {
-      subject: 'Economy', A: 8.3, fullMark: 15,
+      subject: economy.name, A: economy.score_out_of_10, fullMark: 15,
     },
     {
-      subject: 'Leisure & Culture', A: 5.4, fullMark: 15,
+      subject: leisure.name, A: leisure.score_out_of_10, fullMark: 15,
     },
     {
-      subject: 'Tolerance', A: 4, fullMark: 15,
+      subject: tolerance.name, A: tolerance.score_out_of_10, fullMark: 15,
     },
     {
-      subject: 'Outdoor', A: 7, fullMark: 15,
+      subject: outdoor.name, A: outdoor.score_out_of_10, fullMark: 15,
     },
 
   ];
