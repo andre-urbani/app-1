2ab2254957136f1910138b571812ea62f6ec1064
dredizzle <dredizzle@live.co.uk>
2020-04-09 13:44:32 +0100

    cleaning code

--- src/components/City.js
+++ src/components/City.js
@@ -3,42 +3,12 @@ import axios from 'axios'
 
 const City = (props) => {
 
-  const image = 'ua:images'
+  const [city, setCity] = useState([])
 
-  const [urbanArea, setUrbanArea] = useState([])
-
-  const [imageJson, setImageJson] = useState([])
-
-  // const [test, setTest] = useState([])
-
-
-
-  useEffect(() => {
-    axios.get(props.location.target)
-      .then(res => {
-        const uaList = res.data._links
-        setUrbanArea(uaList)
-        axios.get(res.data._links[image].href)
-          .then(res => {
-            setImageJson(res.data.photos[0].image)
-          })
-      })
-  }, [])
+  
 
   return <div>
-    {imageJson ?
-
-      <div><img src={imageJson.mobile} />
-      </div> :
-      null}
-    <RadarChartTest urbanArea={urbanArea} />
-    {/* <SvgChartTest urbanArea={urbanArea}/> */}
- 
-    {/* {test ?
-
-      <div>hello
-      </div> :
-      null} */}
+    
   </div>
 
 
