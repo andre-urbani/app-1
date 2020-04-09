ab533435f8a96ca32520c10a039a786dbedd1582
dredizzle <dredizzle@live.co.uk>
2020-04-09 13:42:20 +0100

    cleaning code

---
+++ src/components/City.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import RadarChartTest from './RadarChartTest';
+
+const UrbanArea = (props) => {
+
+  const image = 'ua:images'
+
+  const [urbanArea, setUrbanArea] = useState([])
+
+  const [imageJson, setImageJson] = useState([])
+
+  // const [test, setTest] = useState([])
+
+
+
+  useEffect(() => {
+    axios.get(props.location.target)
+      .then(res => {
+        const uaList = res.data._links
+        setUrbanArea(uaList)
+        axios.get(res.data._links[image].href)
+          .then(res => {
+            setImageJson(res.data.photos[0].image)
+          })
+      })
+  }, [])
+
+  return <div>
+    {imageJson ?
+
+      <div><img src={imageJson.mobile} />
+      </div> :
+      null}
+    <RadarChartTest urbanArea={urbanArea} />
+    {/* <SvgChartTest urbanArea={urbanArea}/> */}
+ 
+    {/* {test ?
+
+      <div>hello
+      </div> :
+      null} */}
+  </div>
+
+
+
+}
+
+export default UrbanArea
