583b83554c50520c0bd9423d9bf0f6aad73b047a
dredizzle <dredizzle@live.co.uk>
2020-05-07 16:10:57 +0100

    city page

--- src/components/City.js
+++ src/components/City.js
@@ -7,17 +7,15 @@ const City = (props) => {
   
   const uArea = 'city:urban_area'
 
-  const [urbanArea, setUrbanArea] = useState()
+  const [urbanArea, setUrbanArea] = useState([])
 
   useEffect(() => {
     axios.get(props.location.target)
       .then(res => {
         const cityList = res.data._links
         setCity(cityList)
-        // axios.get(props.location.target._links[uArea])
-        //   .then(res => {
-        //     setUrbanArea(res.data.href)
-        // })
+        setUrbanArea(cityList[uArea])
+        
       })
   }, [])
   
@@ -25,7 +23,7 @@ const City = (props) => {
   return <div>
     hello
     {/* {city} */}
-    {console.log(city[uArea])}
+    {console.log(urbanArea.href)}
   </div>
 
 
